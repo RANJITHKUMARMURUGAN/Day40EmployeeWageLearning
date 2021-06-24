432e0fd2a84e2cb704031d69523652cea8f9a7b6
unknown <ranjithm1193@gmail.com>
2021-06-24 18:07:47 +0530

    [Ranjith] added UC4forloop.js

---
+++ UC4forloop.js
@@ -0,0 +1,25 @@
+const IS_PART_TIME = 1;
+ const IS_FULL_TIME = 2;
+ const PART_TIME_HOURS = 4;
+ const FULL_TIME_HOURS = 8;
+ const WAGE_PER_HOURS = 20;
+ const NUM_OF_WORKING_DAY = 20;
+ 
+ function getWorkingHours(empCheck){
+    switch(empCheck){
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+        case IS_FULL_TIME:
+             return FULL_TIME_HOURS;
+             default:
+                return 0;
+    }
+ }
+
+let totalEmpHrs = 0;
+for(let day = 0;day<NUM_OF_WORKING_DAY;day++){
+    let empCheck = Math.floor(Math.random()*10)%3;
+    totalEmpHrs += getWorkingHours(empCheck);
+}
+let empWage = totalEmpHrs*WAGE_PER_HOURS;
+console.log("Hour:"+" "+totalEmpHrs+" "+"Employee Wages is: "+" "+empWage);
